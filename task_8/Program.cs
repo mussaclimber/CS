a03240633a66bee8d51f01d91b333b05449cd696
Mussa <dzhu.ma@icloud.com>
2022-12-10 22:17:12 +0300

    task_8

---
+++ task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int m = 1;
+int i = 2;
+for ( i = m; i <= n; i++)
+if ( i % 2 == 0)
+{
+    Console.WriteLine(i);
+}
+    Console.WriteLine("Это все четные числа из введенного числа");
