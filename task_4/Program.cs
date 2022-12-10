179778171581d2060dee6207280a8f0c597b5ec2
Mussa <dzhu.ma@icloud.com>
2022-12-10 21:56:00 +0300

    task_4

---
+++ task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int numC = int.Parse(Console.ReadLine());
+
+if (numA > numB && numA > numC)
+{
+	Console.WriteLine("Первое число самое больше");
+}
+else if (numB > numA && numB > numC)
+{
+	Console.WriteLine("Второе число самое больше");
+}
+else if (numC > numA && numC > numB)
+{
+	Console.WriteLine("Третье число самое больше");
+}
