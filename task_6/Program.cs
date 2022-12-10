c5cdfe3748cd75051e94b5f3ab25f31025b8af1e
Mussa <dzhu.ma@icloud.com>
2022-12-10 22:03:04 +0300

    task_6

---
+++ task_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+if (n % 2 == 0)
+{
+	Console.WriteLine("Число является четным");
+} 
+else
+{
+	Console.WriteLine("Число является нечетным, введите иное число.");
+}
