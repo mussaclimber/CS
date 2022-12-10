89bbe9f978c142d45bf48278a7fbdbdaa514270d
Mussa <dzhu.ma@icloud.com>
2022-12-10 21:38:10 +0300

    task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Clear();
+
+Console.Write("Первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Второе число: ");
+int numB = int.Parse(Console.ReadLine());
+    if (numA > numB) 
+    {
+	    Console.WriteLine("Первое число больше");
+    }
+    else 
+    {
+	    Console.WriteLine("Второе число больше");
+    }
+
+
+// Если введут два одинаковых числа
+    /*
+    
+Console.Write("Первое число: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Второе число: ");
+int numB = int.Parse(Console.ReadLine());
+ 
+    if (numA > numB)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+     else if (numA < numB)
+     {
+                Console.WriteLine("Первое число меньше второго");
+    }
+    else
+    {
+        Console.WriteLine("Оба числа равны");
+    }
+    */
