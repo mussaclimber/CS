82f786c1d63443a71692a338ce0a15b7d43e2f03
Mussa <dzhu.ma@icloud.com>
2022-12-17 12:14:21 +0300

    test_2clear

---
+++ task_13/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число чтобы узнать третью цифру");
+int num = int.Parse(Console.ReadLine());
+int numA = num;
+
+while (num < 99)
+    {
+        Console.WriteLine("третьей цифры нет");
+		  Console.WriteLine("Введите другое число: ");
+		  num = int.Parse(Console.ReadLine());
+    }
+    {
+         if (num >= 100 && num <= 999)
+			{
+		  	Console.WriteLine($"Третья цифра {num % 10}");
+			}
+			else
+			{
+				while (num > 999)
+					num = num / 10;
+					Console.WriteLine($"Третья цифра {num % 10}");
+			}
+    }
+
