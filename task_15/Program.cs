b22abc07b54a309097c91b5cdbc4ccc97480f911
Mussa <dzhu.ma@icloud.com>
2022-12-17 13:08:51 +0300

    task_15

---
+++ task_15/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Введите порядковый номер дня недели");
+        int num = int.Parse(Console.ReadLine());
+
+        if (num > 0 && num < 8)
+        {
+            if (num == 7 || num == 6) // при условии если первый день недели понедельник
+            {
+                Console.Write($"{num} - это выходной день");
+            }
+            else
+            {
+                Console.Write($"{num} - это рабочий день");
+            }
+        }
+        else
+        {
+            Console.Write(" введите число в пределах от 1 до 7 включительно");
+        }
+    }
+}
