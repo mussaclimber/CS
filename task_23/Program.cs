da122956d7e21d7eef3dc354a6ebedd88d236f2e
Mussa <dzhu.ma@icloud.com>
2022-12-23 17:05:40 +0300

    task_23

---
+++ task_23/Program.cs
@@ -0,0 +1,15 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+        int i = 1;
+        double sum = 0;
+        
+            for (i = 1; i <= n; i++)
+            Console.WriteLine(sum = Math.Pow(i, 3));
+    }
+}
