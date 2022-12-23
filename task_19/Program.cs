a7526d8e855e185e5b3ab5f720852912472ffbc5
Mussa <dzhu.ma@icloud.com>
2022-12-23 15:42:29 +0300

    task_19

---
+++ task_19/Program.cs
@@ -0,0 +1,25 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Введите пятизначное число: ");
+        int number = int.Parse(Console.ReadLine());
+        while (number < 9999 || number > 100000)
+        {
+            Console.Write("Введите пятизначное число: ");
+            number = int.Parse(Console.ReadLine());
+        }
+        string palindrome = number.ToString();
+
+        if (palindrome[0] == palindrome[4] && palindrome[1] == palindrome[3])
+        {
+            Console.WriteLine("Число явлется палиндромом!");
+        }
+        else
+        {
+            Console.WriteLine("Число неявлется палиндромом!");
+        }
+    }
+}
