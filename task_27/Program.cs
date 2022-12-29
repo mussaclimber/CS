074beb3b0729c1b6dbb4cd0e14a53ce72a9c7a48
Mussa <dzhu.ma@icloud.com>
2022-12-29 22:46:24 +0300

    task_27

---
+++ task_27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        string numberString = number.ToString();
+        int sumNumber = 0;
+
+            for (int i = 0; i < numberString.Length; i++)
+            {
+                sumNumber = sumNumber + Convert.ToInt32(numberString[i].ToString());
+            }
+
+        Console.WriteLine(sumNumber);
+    }
+}
